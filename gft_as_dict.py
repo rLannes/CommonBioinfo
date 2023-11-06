cf8eaf88955142fc480517d744e423f586f165f8
rLannes <romain.lannes@gmail.com>
2023-11-06 09:46:03 -0500

    Update gft_as_dict.py

--- gft_as_dict.py
+++ gft_as_dict.py
@@ -12,7 +12,7 @@ def get_attr(string):
     return dico
 
 
-def gtf_as_dict(gtf_file, out=None):
+def gtf_as_dict(gtf_file, biot_field, trans_s_field, gene_s_field, out=None):
 
     dico = {}
     with open(gtf_file) as f_in:
@@ -28,7 +28,8 @@ def gtf_as_dict(gtf_file, out=None):
             strand =  spt[6]
             attr = get_attr(spt[-1])
             gene_id = attr["gene_id"]
-            gene_symbol = attr["gene_symbol"]
+            gene_symbol = attr[gene_s_field]
+            biotype =  
             type_ = spt[2]
 
             if type_ == "gene":
@@ -39,13 +40,13 @@ def gtf_as_dict(gtf_file, out=None):
                     "end" : end,
                     "strand" : strand,
                     "symbol" :  gene_symbol,
-                    "biotype" : dd_bio.get(gene_id, "unknown"),
+                    "biotype" : attr.get(biot_field, "NA"),
                 "transcript": {}}
 
             else:
 
                 transcript_id  = attr["transcript_id"]
-                transcript_symbol = attr["transcript_symbol"]
+                transcript_symbol = attr[trans_s_field]
 
                 if transcript_id not in dico[gene_id]["transcript"]:
                     dico[gene_id]["transcript"][transcript_id] = {
@@ -69,12 +70,18 @@ def gtf_as_dict(gtf_file, out=None):
     return dico
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--gtf', "-gtf")   
-  parser.add_argument('--out', '-out')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gtf', "-gtf")   
+    parser.add_argument('--out', '-out')
+    parser.add_argument('-biotype', default="gene_biotype")
+    parser.add_argument('-trans_symbol', default="transcript_name")
+    parser.add_argument('-gene_symbol', default="gene_name")
+    # parser.add_argument('-biotype', default="gene_biotype")
+    # parser.add_argument('-biotype', default="gene_biotype")
+    
   args = parser.parse_args()
   out = None
   if args.out:
     out = args.out
-  gtf_as_dict(args.gtf, out)
+  gtf_as_dict(args.gtf, args.main_f , out, biot_field=args.biotype, trans_s_field=args.trans_symbol, gene_s_field=args.gene_symbol)
   
